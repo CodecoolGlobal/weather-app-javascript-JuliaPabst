c18aa263416acbccfc9d79f2040c5a912f837157
Mehmet <mehmet.ali.yilmaz1711@gmail.com>
2023-03-21 15:52:01 +0100

    updated changable cities

--- index.js
+++ index.js
@@ -1,40 +1,73 @@
-
 const root = document.querySelector("#root");
-
-
-const createFormWithInput = () => {
-    const form = document.createElement("form");
-    form.setAttribute("autocomplete", "on");
-    form.setAttribute("method", "get");
-
-    const input = document.createElement("input");
-    input.setAttribute("placeholder", "Country Name");
-    form.appendChild(input);
-
-    return form;
-};
-
-const insertFormWithInputElement = () => {
-    root.appendChild(createFormWithInput());
-};
-
-insertFormWithInputElement();
-
 const input = document.querySelector("input");
-console.log(input);
-input.addEventListener("submit", event => {
-    event.preventDefault();
-});
+let sellectedCountry = 'Vienna';
+
+const createFormWithInput = (cityName) => {
+    const option = document.createElement("option");
+    option.setAttribute("value", cityName);
+
+    return option;
+};
+
+const insertOptionElement = (cityName) => {
+    const cities = document.getElementById("cities");
+    cities.appendChild(createFormWithInput(cityName));
+};
 
 const cityNames = cities.map(city => city.name);
-console.log(cityNames);
+cityNames.forEach(city => {
+    insertOptionElement(city);
+});
 
-fetch("https://api.weatherapi.com/v1/current.json?key=ef3f93021b5549f6866100215232103&q=global")
+const createImage = (url) => {
+    const img = document.createElement("img");
+    img.src = url;
+
+    root.appendChild(img);
+};
+
+const insertCelsius = (watherInCelsius, name, url) => {
+    const countryName = document.createElement("h1");
+    countryName.innerText = name;
+    root.appendChild(countryName);
+
+    createImage(url);
+
+    const div = document.createElement("div");
+    div.setAttribute("id", "celsius");
+    div.innerText = `${watherInCelsius} °Celsius`
+
+    root.appendChild(div);
+};
+
+fetch(`https://api.weatherapi.com/v1/current.json?key=ef3f93021b5549f6866100215232103&q=${sellectedCountry}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        const url = data.url;
-        const weather = data;
+        const image = "https:" + data.current.condition.icon
+
+        insertCelsius(data.current.feelslike_c, data.location.name, image);
+
+        input.addEventListener("keypress", event => {
+
+            if(event.key === "Enter") {
+                sellectedCountry = event.target.value;
+                document.querySelector("h1").remove();
+                document.querySelector("#celsius").remove();
+                document.querySelector("img").remove();
+
+                fetch(`https://api.weatherapi.com/v1/current.json?key=ef3f93021b5549f6866100215232103&q=${sellectedCountry}`)
+                .then(response => response.json())
+                .then(data => {
+                    const image = "https:" + data.current.condition.icon
+
+                    insertCelsius(data.current.feelslike_c, data.location.name, image);
+                });
+            }
+        });
+
+        // console.log(data.location.name);
+
+        // console.log(data.current.feelslike_c);
     });
 
 // // replace YOUR_API_KEY with your Weather API key
@@ -54,3 +87,11 @@ fetch("https://api.weatherapi.com/v1/current.json?key=ef3f93021b5549f68661002152
 //     // handle errors
 //     console.error(error);
 //   });
+
+window.addEventListener("load", event => {
+    input.value = '';
+});
+
+window.addEventListener("click", event => {
+    console.log(event.target);
+});
