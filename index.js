537b64bb0a3b331ad1e9e67c1f2af24a3e7abb18
Jules Pabst <ju.pabst@gmail.com>
2023-03-23 15:15:46 +0100

    design

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const searchApiUrl = `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=
 const currentApiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`;
 const pexelsApiKey = "uwcHOUHm37YDeAWCiRkUhKxaBNBKN0HvGcxtZlb1Y7h7EfGRX7PK0dbK";
 
-let selectedCity = 'Vie';
+let selectedCity = "Vie";
 
 const createFormWithInput = (cityName) => {
-    const option = document.createElement("option");
-    option.setAttribute("value", cityName);
+  const option = document.createElement("option");
+  option.setAttribute("value", cityName);
 
-    return option;
+  return option;
 };
 
 const insertOptionElement = (cityName) => {
@@ -34,75 +34,92 @@ const createImage = (url) => {
 };
 
 const insertWeatherData = (weatherInCelsius, weatherInFahrenheit, name) => {
-    
-    const countryName = document.createElement("h1");
-    countryName.innerText = name;
-    countryName.setAttribute("class", "location");
-    chosenWeather.appendChild(countryName);
+  const countryName = document.createElement("h1");
+  countryName.innerText = name;
+  countryName.setAttribute("class", "location");
+  chosenWeather.appendChild(countryName);
 
-    const temperatureInfo = document.createElement("div");
-    temperatureInfo.setAttribute("id", "temps");
-    temperatureInfo.setAttribute("class", "temp");
-    temperatureInfo.innerText = `${weatherInCelsius}° C | ${weatherInFahrenheit}° F`;
+  const temperatureInfo = document.createElement("div");
+  temperatureInfo.setAttribute("id", "temps");
+  temperatureInfo.setAttribute("class", "temp");
+  const lineBreak = document.createElement("hr");
+  temperatureInfo.innerText = `${weatherInCelsius}° C | ${weatherInFahrenheit}° F`;
 
-    chosenWeather.appendChild(temperatureInfo);
+  chosenWeather.appendChild(lineBreak);
+  chosenWeather.appendChild(temperatureInfo);
 };
 
-const insertAdditionalLocationData = (humidity, uv, windSpeed, windDirection) => {
-    const otherInfo = document.createElement("div");
-    otherInfo.setAttribute("id", "other");
-    otherInfo.setAttribute("class", "otherInfo");
-    otherInfo.innerText = `Humidity: ${humidity} | UV: ${uv} | Wind: ${windSpeed}km/h (${windDirection})`;
+const insertAdditionalLocationData = (
+  humidity,
+  uv,
+  windSpeed,
+  windDirection
+) => {
+  const otherInfo = document.createElement("div");
+  otherInfo.setAttribute("id", "other");
+  otherInfo.setAttribute("class", "otherInfo");
+  otherInfo.innerText = `Humidity: ${humidity} | UV: ${uv}| Wind: ${windSpeed}km/h (${windDirection})`;
 
-    chosenWeather.appendChild(otherInfo);
+  chosenWeather.appendChild(otherInfo);
 };
 
 fetch(searchApiUrl)
-    .then(response => response.json())
-    .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
+    input.addEventListener("input", (event) => {
+      let selectedCity = event.target.value;
 
-        input.addEventListener("input", event => {
-            let selectedCity = event.target.value;
-
-            fetch(searchApiUrl + selectedCity)
-                .then(response => response.json())
-                .then(data => {
-                    datalistCities.replaceChildren();
-                    if( data.length > 0) {
-                        data.forEach(city => {
-                            insertOptionElement(city.name);
-                        });
-                    }
-                })
+      fetch(searchApiUrl + selectedCity)
+        .then((response) => response.json())
+        .then((data) => {
+          datalistCities.replaceChildren();
+          if (data.length > 0) {
+            data.forEach((city) => {
+              insertOptionElement(city.name);
+            });
+          }
         });
     });
 
-        input.addEventListener("keypress", event => {
-            if(event.key === "Enter") {
-                fetch(currentApiUrl + event.target.value)
-                    .then(response => response.json())
-                    .then(data => {
-                        const cityName = data.location.name; 
+    input.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        fetch(currentApiUrl + event.target.value)
+          .then((response) => response.json())
+          .then((data) => {
+            const cityName = data.location.name;
 
-                        // const image = "https:" + data.current.condition.icon; ==> TODO: EVTL. ERSETZEN MIT ICONS VIA DISCORD-LINK
+            // const image = "https:" + data.current.condition.icon; ==> TODO: EVTL. ERSETZEN MIT ICONS VIA DISCORD-LINK
 
-                        fetch(`https://api.pexels.com/v1/search?query=${cityName}`, {headers:{Authorization: pexelsApiKey}})
-                            .then(response => response.json())
-                            .then(data => {
-                                const cityImage = data.photos[0].src.landscape; 
-        
-                                body.setAttribute("style", `background-image: url(${cityImage})`);
-                        });
+            fetch(`https://api.pexels.com/v1/search?query=${cityName}`, {
+              headers: { Authorization: pexelsApiKey },
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                const cityImage = data.photos[0].src.landscape;
 
-                        chosenWeather.replaceChildren();
-                        insertWeatherData(data.current.feelslike_c, data.current.feelslike_f, data.location.name);
-                        insertAdditionalLocationData(data.current.humidity, data.current.uv, data.current.wind_kph, data.current.wind_dir);
-                    })
-            }
-        });
+                body.setAttribute(
+                  "style",
+                  `background-image: url(${cityImage})`
+                );
+              });
+
+            chosenWeather.replaceChildren();
+            insertWeatherData(
+              data.current.feelslike_c,
+              data.current.feelslike_f,
+              data.location.name
+            );
+            insertAdditionalLocationData(
+              data.current.humidity,
+              data.current.uv,
+              data.current.wind_kph,
+              data.current.wind_dir
+            );
+          });
+      }
     });
   });
 
-window.addEventListener("load", event => {
-    input.value = '';
+window.addEventListener("load", (event) => {
+  input.value = "";
 });
